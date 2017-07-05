684a8b579b4fb0f27c73d7b76c7af4a0bf915023
ICELI <woticeli@gmail.com>
2017-07-05 21:30:35 +0800

    refactor: 简化

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # upload
 uploadify 迁移至 fine-uploader
+
+迁移说明：
+
+1. 文件引用 
+
+附件获取以下js
+ - fine-uploader.core.js
+ - upload.js
+
+替换原来的uploadify
+<! uploadify start >
+<link rel="stylesheet" href="http://image5.jm.com/dsw/js/uploadify/css/uploadify.css"/>
+<link rel="stylesheet" href="http://image5.jm.com/dsw/js/uploadify/css/upload-custom.css"/>
+<script src="http://image5.jm.com/dsw/js/uploadify/scripts/swfobject.js"></script>
+<script src="http://image5.jm.com/dsw/js/uploadify/scripts/jquery.uploadify.v2.1.0.js"></script>
+<script src="http://image5.jm.com/dsw/js/upload.js"></script>
+<! uploadify end >
+
+2. api变动
+ - buttonImg 弃用  按钮样式由用户定义 
+ - fileQueue 弃用  文件上传队列动画
+ - queueSizeLimit 弃用
+ - simUploadLimit 弃用
+
+ - fileInputTitle 新增 按钮title
+ - webFilePath 默认读取全局常量 WEB_FILE_PATH，用于统一配置上传服务器地址，不需要每次都传了，也可继续传参覆盖。
+ - webViewPath 默认读取全局常量 WEB_VIEW_PATH，用于统一配置查看图片服务器地址。
+ - fine-uploader所有回调参数不带event对象，如原先的onSelect(event, queueID, fileObj)，将获取不到event，参数改为控件的实例
+
+3. 注意细节
+ - fileUpload()初始化控件方式不变, 但`uploadId`改为包装上传按钮的父容器，不能为input等自闭合标签。 （考虑到老控件一直以input作为uploadId，已添加判断：如果uploadId是input元素就使用他的父容器作为上传按钮）
+ - 选择回调onSelect(this, queueID, fileObj) 第一个参数改为返回当前上传控件实例。
+ - 回调参数fileObj对象在ie8、9下无法获取文件大小，fileObj.size = -1，导致前端校验失效。
+ - 实例包含大量方法提供用户调用，如取消上传, 获取其他属性。详细文档https://docs.fineuploader.com
+ - 查看api时请注意，目前只使用了fine-uploader的core版，未包含ui模块。
--- upload.js
+++ upload.js
@@ -1,16 +1,17 @@
 /*通过fileExt可以直接指定允许的后缀，通过fileType则可以批量指定允许的后缀，但需要注意指定fileType的时候不会提示允许上传的文件类型，请在placeholder中说明下*/
 var UPLOAD_FILE_TYPE_INFO = {
-	// 图片
-	'image': ['.jpeg','.jpg','.bmp','.gif','.png','.tif','.rgb','.dib','.eps','.jpe','.pcx','.bmp','.gif','.pdf'],
-	// 只允许图片（有此属性时不会再判断其它属性，哪怕逗号分隔定义了多个也不行）
-	'only-image': ['.jpeg','.jpg','.bmp','.gif','.png','.tif','.rgb','.dib','.eps','.jpe','.pcx','.bmp','.gif','.pdf'],
-	// 文档
-	'doc': ['.doc', '.docx', '.pdf'],
-	// 只允许文档（有此属性时不会再判断其它属性，哪怕逗号分隔定义了多个也不行）
-	'only-doc': ['.doc', '.docx', '.pdf'],
-	// excel
-	'excel': ['.xls', '.xlsx', '.pdf']
-}
+    // 图片
+    'image': ['.jpeg','.jpg','.bmp','.gif','.png','.tif','.rgb','.dib','.eps','.jpe','.pcx','.bmp','.gif','.pdf'],
+    // 只允许图片（有此属性时不会再判断其它属性，哪怕逗号分隔定义了多个也不行）
+    'only-image': ['.jpeg','.jpg','.bmp','.gif','.png','.tif','.rgb','.dib','.eps','.jpe','.pcx','.bmp','.gif','.pdf'],
+    // 文档
+    'doc': ['.doc', '.docx', '.pdf'],
+    // 只允许文档（有此属性时不会再判断其它属性，哪怕逗号分隔定义了多个也不行）
+    'only-doc': ['.doc', '.docx', '.pdf'],
+    // excel
+    'excel': ['.xls', '.xlsx', '.pdf']
+};
+
 /**
  * 初始化uploadify上传
  * @param imgId 上传成功后 显示上传图片的 img标签的ID
@@ -19,8 +20,8 @@ var UPLOAD_FILE_TYPE_INFO = {
  * @param fileType 文件类型 可选 ("file","image","doc") 多个可使用 "," 分隔
  */
 function fileUpload(param) {
-    var language = 'cn';
     var token = '';
+    var language = 'cn';
     var domain = '';
     var _fileObj = {};
 
@@ -28,19 +29,20 @@ function fileUpload(param) {
         uploadId: '',
         imgId: '',
         filePathId: '',
-        webFilePath: fileUploadAddress,
+        webFilePath: window.WEB_FILE_PATH || '',
+        webViewPath: window.WEB_VIEW_PATH || '',
         fileType: 'image',
         fileSize: 5,
         ifyWidth: 50,
         ifyHeight: 31,
-        buttonImg: staticUrl + '/static/img/upload-bg.png',
         fileQueueAuto: true,
         fileMulti: false,
         maxImageSize: 200,
         fileExt: null,
-        callback: function() {},
-        onSelect: function() {},
+        callback: null,
+        onSelect: null,
         // 弃用api
+        buttonImg: '',
         fileQueue: 'fileQueue',
         queueSizeLimit: 6,
         simUploadLimit: 1,
@@ -50,31 +52,38 @@ function fileUpload(param) {
 
     var option = $.extend({}, defaults, param);
 
+
+    var button = typeof option.uploadId === 'string'
+        ? document.getElementById(option.uploadId)
+        : option.uploadId;
+
+    if(!$(button).length) {
+        return false;
+    }
+    qq.log($(button)[0].nodeName, option.uploadId);
+    button = $(button)[0].nodeName.toLowerCase() === 'input' ? button.parentNode : button;
+
     $.getJSON('/getUploadToken?t=' + (+new Date()), function(data) {
-        var obj = eval('(' + data + ')');
+        data = JSON.parse(data);
 
-        if (obj.uploadToken) {
-            language = obj.language;
-            token = obj.uploadToken;
-            domain = obj.domain;
-
-            var button = typeof option.uploadId === 'string'
-                ? document.getElementById(option.uploadId)
-                : option.uploadId;
+        if (data.uploadToken) {
+            token = data.uploadToken;
+            language = data.language;
+            domain = data.domain;
 
             new qq.FineUploaderBasic({
                 request: {
                     endpoint: option.webFilePath + '/upload.do?action=upload',
-					params: {
-						'moduleFlag': 'report',
-						'token': token,
-						'domain': domain,
-						'language': language,
-						'fileType': option.fileType,
-						'fileSize': option.fileSize,
-						'maxImageSize': option.maxImageSize,
-                        'addScript': (button.style.transition === undefined ? 'true' : '')
-					},//后台参数 json格式
+                    params: {
+                        moduleFlag: 'report',
+                        token: token,
+                        domain: domain,
+                        language: language,
+                        fileType: option.fileType,
+                        fileSize: option.fileSize,
+                        maxImageSize: option.maxImageSize,
+                        addScript: (button.style.transition === undefined ? 'true' : '')
+                    },//后台参数 json格式
                     uuidName: 'uuid'
                 },
                 autoUpload: option.fileQueueAuto,
@@ -129,20 +138,17 @@ function fileUpload(param) {
                         }
 
                         if ($('.file-item').find('p').length >= 5) {
-                            layer.alert('Maximum allowed upload 5 attached', { icon: 2 });
+                            // layer.alert('Maximum allowed upload 5 attached', { icon: 2 });
                             layer.alert('上传的文件个数不得超过5个', { icon: 2 });
-                            this.cancel(id);
                             return false;
                         }
 
                         if (fileObj.size > option.fileSize * 1024 * 1024) {
                             layer.alert('上传文件不允许大于' + option.fileSize + 'M', { icon: 2 });
-                            this.cancel(id);
                             return false;
                         }
 
                         if (!validateFileType(fileObj, option)) {
-                            this.cancel(id);
                             return false;
                         }
 
@@ -167,63 +173,55 @@ function fileUpload(param) {
                         var obj = responseJSON;
 
                         $.cookie('JSESSIONID', token);//跨域传输之后必须设置cookie 否则会丢失此次的session
-                        // var obj = eval('(' + _path + ')');
-                        $.getJSON('/getFile?uuid=' + obj.fileUUIDs[0],
-                            function(data) {
-                                var callback = option.callback;
 
-                                $(button).
-                                    siblings('.poptip').
-                                    remove().
-                                    end().
-                                    removeClass('red-border');
-                                var obj = eval('(' + data + ')');
-                                obj.size = _fileObj.size;
-                                obj.type = _fileObj.type;
-                                obj.imgId = option.imgId;
-                                obj.filePathId = option.filePathId;
-                                //执行回调函数 判断是否有回调函数,如果没有就默认执行
-                                if (callback && callback != undefined &&
-                                    typeof callback === 'string') {
-                                    callback = window[callback];
-                                    if (typeof callback === 'function') {
-                                        callback.apply([], [obj]);
-                                    }
-                                } else if (isFunction(callback)) {//判断是否是function
-                                    callback(obj);
-                                } else {
-                                    //判断是否是图片格式
-                                    if (!$.inArray(obj.type, UPLOAD_FILE_TYPE_INFO['image']) >= 0) {
-                                        if (obj && obj.resultCode == 4) {
-                                            var filePaths = obj.filePaths;//文件查看路径
-                                            var originalFileNames = obj.originalFileNames;//原文件名
+                        $.getJSON('/getFile?uuid=' + obj.fileUUIDs[0], function(data) {
+                            var callback = option.callback;
+                            var obj = JSON.parse(data);
 
-                                            for (var i = 0; i < filePaths.length; i++) {
-                                                $('#' + option.imgId).attr('src', option.webFilePath + '/' + filePaths[i]);
-                                                $('#' + option.filePathId).val(filePaths[i]);
-                                            }
-                                        } else {
-                                            layer.alert(obj.resultMsg, { icon: 2 });
+                            obj.size = _fileObj.size;
+                            obj.type = _fileObj.type;
+                            obj.imgId = option.imgId;
+                            obj.filePathId = option.filePathId;
+                            //执行回调函数 判断是否有回调函数,如果没有就默认执行
+                            if (callback && callback != undefined && typeof callback === 'string') {
+                                callback = window[callback];
+                                if (typeof callback === 'function') {
+                                    callback.apply([], [obj]);
+                                }
+                            } else if (isFunction(callback)) {
+                                //判断是否是function
+                                callback(obj);
+                            } else {
+                                //判断是否是图片格式
+                                if (!$.inArray(obj.type, UPLOAD_FILE_TYPE_INFO['image']) >= 0) {
+                                    if (obj && obj.resultCode == 4) {
+                                        var filePaths = obj.filePaths;//文件查看路径
+
+                                        for (var i = 0; i < filePaths.length; i++) {
+                                            $('#' + option.imgId).attr('src', option.webViewPath + '/' + filePaths[i]);
+                                            $('#' + option.filePathId).val(filePaths[i]);
                                         }
+                                    } else {
+                                        layer.alert(obj.resultMsg, { icon: 2 });
                                     }
                                 }
-                            });
+                            }
+                        });
                     },
                     onUpload: function() {
                         qq.log('=============onUpload', arguments);
                     }
-				}
-			});
-
+                }
+            });
         }
     });
 }
 
 function validateFileType(fileObj, settings) {
-	var objType = '.'+ fileObj.name.toLowerCase().split('.').pop();
-	var fileExt = settings.fileExt || '';
-	var fileType = settings.fileType || 'image';
-	if(fileExt){
+    var objType = '.'+ fileObj.name.toLowerCase().split('.').pop();
+    var fileExt = settings.fileExt || '';
+    var fileType = settings.fileType || 'image';
+    if(fileExt){
         var ret = false;
         fileExt = fileExt.toLowerCase();
         var extArr = fileExt.split(",");
@@ -236,17 +234,17 @@ function validateFileType(fileObj, settings) {
         }
         return ret;
     } else if(fileType) {
-    	var fileTypes = fileType.split(',');
-    	for(var i = 0; i < fileTypes.length; i++) {
-    		var accepts = UPLOAD_FILE_TYPE_INFO[$.trim(fileTypes[i])];
-    		if($.inArray(objType, accepts) >= 0) {
-    			return true;
-    		}
-    	}
-    	layer.alert("文件类型不正确", {icon: 2});
-    	return false;
+        var fileTypes = fileType.split(',');
+        for(var i = 0; i < fileTypes.length; i++) {
+            var accepts = UPLOAD_FILE_TYPE_INFO[$.trim(fileTypes[i])];
+            if($.inArray(objType, accepts) >= 0) {
+                return true;
+            }
+        }
+        layer.alert("文件类型不正确", {icon: 2});
+        return false;
     } else {
-    	// 默认只允许图片
+        // 默认只允许图片
         if(!$.inArray(objType, UPLOAD_FILE_TYPE_INFO['image']) >= 0){
             layer.alert("文件类型不正确", {icon: 2});
             return false;
@@ -257,221 +255,6 @@ function validateFileType(fileObj, settings) {
 
 function isFunction(fn){
     return Object.prototype.toString.call(fn) === '[object Function]';
-}
-
-//显示图片
-var IMAGE_DEFAULTS = {
-		fileViewAddress: fileViewAddress, // 一般不需要传，默认就是文件查看的服务器地址
-		showDel: false, // 图片是否可删除
-		showView: true, // 图片是否可查看大图
-		showEdit: true, // 图片是否可编辑
-		showClear: false, // 图片是否可清除，注意showClear和showDel同时存在时，showDel是无效的
-		editable: true, // 是否有修改的权限，如果为false，会自动把showEdit、showDel设为false，并且max设为1
-		inputHidden: undefined, // 隐藏的input框，用于存放图片地址
-		max: 1, // 上传图片张数，设置大于1时需要在外层用image-upload-group包住
-		callback: null
-};
-/**
- * 显示图片
- *
- * @param id file控件的id
- * @param data 图片信息，{filePaths: ["xxx.jpg"], originalFileNames: ["文件名"]}
- * @param 参数，参考IMAGE_DEFAULTS
- */
-function showImage(id, data, params){
-	var options = $.extend({}, IMAGE_DEFAULTS, params);
-	var fileViewAddress = options.fileViewAddress,
-	showDel = options.showDel,
-	showView = options.showView,
-	showEdit = options.showEdit,
-	showClear = options.showClear,
-	editable = options.editable,
-	inputHidden = options.inputHidden || $('#'+id).siblings("input[name='"+id+"']"),
-	max = options.max,
-	callback = options.callback,
-	btns = options.btns;
-
-	// 当前没有图片，直接初始化图片上传组件
-	if(!data || !data.filePaths || data.filePaths.length==0 || !data.filePaths[0] || data.filePaths[0].indexOf('no_available_url') > 0) {
-		if(editable) {
-			fileUpload({
-				uploadId : id + '_head',//上传组件id
-                fileMulti: options.fileMulti,
-				callback : function(data){
-					if(callback) {
-						callback(data, params);
-					}
-					showImage(id, data, params);
-				}
-			});
-		}
-		return;
-	}
-
-	if(!editable) {
-		showDel = options.showDel = false;
-		showEdit = options.showEdit = false;
-		max = options.max = 1;
-	}
-
-	$(this).remove();
-	var imagePath = data.filePaths[0];
-	var realName = data.originalFileNames ? data.originalFileNames[0] : '';
-	if (imagePath){
-		var url = getFileViewUrl(imagePath);
-		var showImageHead = $("#"+id+"_head");
-		$("#"+id+"_img").remove();
-		//===========
-		showImageHead.find('.change-pic').remove();
-		//===========
-		showImageHead.append("<img src='" + url + "' id='"+id+"_img'  _filePath='" + imagePath + "' _realFileName='" + realName + "'>");
-		// 查看按钮
-	    if(showView) {
-    		showImageBtn(showImageHead, {
-    			name: '查看',
-    			classes: 'show-pic'
-    		});
-    	}
-	    // 编辑按钮
-	    if(showEdit) {
-	    	showImageEditBtn(id, showImageHead, params);
-	    }
-	    // 其它按钮
-	    if(btns) {
-	    	for(var i = 0; i < btns.length; i++) {
-	    		var btn = btns[i];
-	    		showImageBtn(showImageHead, btn);
-	    	}
-	    }
-	    var parent = $('#' + id).parents('.image-upload-group');
-	    var length = parent.find('.image-upload-wrap').length;
-	    if(showClear && $("#"+id+"_head").siblings('.clear-pic').length==0) {
-    		$("#"+id+"_head").before('<i class="clear-pic"></i>');
-    		$("#"+id+"_head").prev('.clear-pic').on('click', function(e){
-    			e.stopPropagation();
-    			$("#"+id).siblings('.change-pic').remove();
-    			$("#"+id).siblings('img').remove();
-    			inputHidden.val('');
-    			$(this).remove();
-    			if($("#"+id).siblings('object').length == 0) {
-    				// 清除后上传组件未初始化的情况
-    				showImage(id, null, params);
-    			}
-    		});
-	    } else if(showDel){
-	    	$("#"+id+"_head").before('<i class="del-pic"></i>');
-		    $("#"+id+"_head").prev('.del-pic').on('click', function(){
-		    	$("#"+id+"_head").parent().remove();
-		    	 if(length == max){
-	    			showImage2(parent, max, inputHidden.attr('name'), fileMulti);
-		    	 }
-		    });
-	    }
-	    parent.attr('data-max', max);
-	    if(max > 1 && parent.find('.image-upload-wrap').length < max) {
-	    	showImage2(parent, max, inputHidden.attr('name'));
-	    }
-
-	    inputHidden.val(imagePath)
-	}else{
-		fileUpload({
-	        uploadId : id + '_head',//上传组件id
-            fileMulti: options.fileMulti,
-	        callback : function(data){
-				showImage(id, data, params);
-	        }
-	    });
-	}
-}
-
-function showImageBtn(showImageHead, btn) {
-	var changePic = showImageHead.find('.change-pic');
-	if(changePic.length == 0) {
-		showImageHead.append('<i class="change-pic"></i>');
-		changePic = showImageHead.find('.change-pic');
-	}
-	var name = btn.name,
-		classes = btn.classes;
-		callback = btn.callback;
-	var btnHtml = $('<em class="' + classes + '">' + name + '</em>');
-	changePic.append(btnHtml);
-	if(callback) {
-		btnHtml.on('click', function() {
-			callback(this);
-		});
-	}
-}
-
-function showImageEditBtn(id, showImageHead, params) {
-	var changePic = showImageHead.find('.change-pic');
-	if(changePic.length == 0) {
-		showImageHead.append('<i class="change-pic"></i>');
-		changePic = showImageHead.find('.change-pic');
-	}
-    var randomId = new Date().getTime() + "_update"; // 随机id，因为id不能重复
-    var btnHtml= $('<em id="'+randomId+'_head"><input type="file" class="file-btn" id="'+randomId+'"/>编辑</em>');
-    changePic.append(btnHtml);
-    fileUpload({
-        uploadId : randomId + '_head',
-        fileMulti: params.fileMulti,
-        callback : function(data){
-            var editParams = $.extend({}, params, {max: 1});
-            showImage(id, data, editParams);
-            if(callback){
-                callback(data, editParams);
-            }
-        }
-    });
-}
-
-//max当前最多可上传的数
-function showImage2(appendComponent, max, name, fileMulti) {
-	//最多可上传图片张数
-	var imageLength = appendComponent.find('.image-upload-wrap').length;
-	var hiddenClass = imageLength == max ? 'hide' : '';
-	var id = new Date().getTime() + "_" + imageLength;
-	var html = $('<span class="image-upload-wrap '+hiddenClass+'">'+
-					'<a class="image-upload" id="'+id+'_head">'+
-				'<input type="file" class="file-btn" id="'+id+'"/>'+
-				'<input type="hidden" name="'+name+'"/>'+
-			'</a>'+
-		'</span>');
-	appendComponent.append(html);
-
-	fileUpload({
-		uploadId : id + '_head',//上传组件id
-        fileMulti: fileMulti,
-	    callback : function(data){
-	    	showImage(id, data, {
-	    		max: max,
-	    		showDel: true,
-	    		inputHidden: html.find('[name='+name+']')
-	    	});
-	    }
-	});
-}
-
-/**
- * 获取图片的完整路径
- *
- * @param url 可以是完整路径也可以是图片服务器的相对路径
- * @param reduce true表示需要压缩，false表示不需要压缩
- * <font color="red"><p>注意只有上传时设置了maxImageSize的图片才可以压缩</p></font>
- */
-function getFileViewUrl(url, reduce) {
-	if(!url) {
-		return '';
-	}
-	if(!/^http:/.test(url)) {
-		url = fileViewAddress + url;
-	}
-	if(reduce === true && url.indexOf('_reduce') === -1) {
-		var index = url.lastIndexOf('.');
-		url = url.substring(0, index) + '_reduce' + url.substring(index);
-	} else if(reduce === false && url.indexOf('_reduce') !== -1) {
-		url = url.replace('_reduce', '');
-	}
-	return url;
 }
 
 /**
